c16b3a3aefbf61d518b7bc4332eba9cd4fbc21d3
Momin Khan <hoopiest.cloth@gmail.com>
2024-03-11 10:45:21 -0700

    Update output.py

--- Lab4/output.py
+++ Lab4/output.py
@@ -2,7 +2,17 @@
 # using namespace std;
 # int main(){
 #     cout<<"Momin Khan";
+    # int a=10;
+    # float b=10.3;
+    # char c='a';
 # }
 
 
+
 print("Momin Khan")
+a = 10
+b=10.3
+c='a'
+print(a,b,c)
+print(type(a))
+print(type(float(a)))
