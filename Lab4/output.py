73366a43db71990aaba7d6d8698078eb3e9965ba
Momin Khan <hoopiest.cloth@gmail.com>
2024-03-11 11:04:09 -0700

    main

--- Lab4/output.py
+++ Lab4/output.py
@@ -10,9 +10,3 @@
 
 
 print("Momin Khan")
-a = 10
-b=10.3
-c='a'
-print(a,b,c)
-print(type(a))
-print(type(float(a)))
---
+++ Lab4/typeCast.py
@@ -0,0 +1,6 @@
+a = 10
+b=10.3
+c='a'
+print(a,b,c)
+print(type(a))
+print(type(float(a)))
