e38fb1d592cbf54d18730a4f942b46f0debf6ff6
Momin Khan <hoopiest.cloth@gmail.com>
2024-03-11 11:31:18 -0700

    main

--- Lab4/commaOperatorInPrint.py
+++ Lab4/commaOperatorInPrint.py
@@ -1,4 +1,13 @@
+# With same data type
+
 a = "Momin"
 b = "Khan"
+
+print(a,b)
+
+
+# with different data type
+a = "Momin"
+b = 12
 
 print(a,b)
--- Lab4/plusOperatorInPrint.py
+++ Lab4/plusOperatorInPrint.py
@@ -1,4 +1,11 @@
+# With same data type
 a = "Momin "
 b = "Khan"
 
 print(a+b)
+
+# with different data type
+a = "Momin"
+b = 12
+
+print(a+b) #error will come 
